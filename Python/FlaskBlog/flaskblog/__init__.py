5d9dc821c2c4be606526ba1b9799c6646947840c
David Ledvinka <David.Ledvinka@cz.abb.com>
2018-09-03 22:07:03 +0200

    Module 06, user authentication

--- Python/FlaskBlog/flaskblog/__init__.py
+++ Python/FlaskBlog/flaskblog/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2j6k5h1j2k1jk1da5s1da6vcdrt'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
 
