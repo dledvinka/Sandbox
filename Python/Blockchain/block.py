b6583afa675bd862dbd1521be0ee0a89a0b705e8
David Ledvinka <ledvinka.david@gmail.com>
2018-12-29 20:27:59 +0100

    section 9 in progress

--- Python/Blockchain/block.py
+++ Python/Blockchain/block.py
@@ -1,10 +1,12 @@
 from time import time
 
+from printable import Printable
 
-class Block:
-    def __init__(self, index, previous_hash, transactions, proof, timestamp=None):
+
+class Block(Printable):
+    def __init__(self, index, previous_hash, transactions, proof, time=time()):
         self.index = index
         self.previous_hash = previous_hash
+        self.timestamp = time
         self.transactions = transactions
-        self.timestamp = time() if timestamp is None else timestamp
         self.proof = proof
