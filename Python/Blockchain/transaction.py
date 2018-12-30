5e672dc746552282ed252ea4fe84eb392c834120
David Ledvinka <ledvinka.david@gmail.com>
2018-12-30 11:07:30 +0100

    in progress

--- Python/Blockchain/transaction.py
+++ Python/Blockchain/transaction.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 
-from printable import Printable
+from utility.printable import Printable
 
 class Transaction(Printable):
     """A transaction which can be added to a block in the blockchain.
--- Python/Blockchain/utility/verification.py
+++ Python/Blockchain/utility/verification.py
@@ -1,4 +1,5 @@
-from hash_util import hash_string_256, hash_block
+"""Provides verification"""
+from utility.hash_util import hash_string_256, hash_block
 
 class Verification:
     """A helper class which offer various static and class-based verification and validation methods."""
