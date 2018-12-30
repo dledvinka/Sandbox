d80949a0f9c7e876ff3d1349539d96f588891439
David Ledvinka <ledvinka.david@gmail.com>
2018-12-30 21:01:49 +0100

    section 10 finished

--- Python/Blockchain/utility/verification.py
+++ Python/Blockchain/utility/verification.py
@@ -1,5 +1,7 @@
-"""Provides verification"""
+"""Provides verification helper methods."""
+
 from utility.hash_util import hash_string_256, hash_block
+from wallet import Wallet
 
 class Verification:
     """A helper class which offer various static and class-based verification and validation methods."""
@@ -35,16 +37,19 @@ class Verification:
         return True
 
     @staticmethod
-    def verify_transaction(transaction, get_balance):
+    def verify_transaction(transaction, get_balance, check_funds=True):
         """Verify a transaction by checking whether the sender has sufficient coins.
 
         Arguments:
             :transaction: The transaction that should be verified.
         """
-        sender_balance = get_balance()
-        return sender_balance >= transaction.amount
+        if check_funds:
+            sender_balance = get_balance()
+            return sender_balance >= transaction.amount and Wallet.verify_transaction(transaction)
+        else:
+            return Wallet.verify_transaction(transaction)
 
     @classmethod
     def verify_transactions(cls, open_transactions, get_balance):
         """Verifies all open transactions."""
-        return all([cls.verify_transaction(tx, get_balance) for tx in open_transactions])
+        return all([cls.verify_transaction(tx, get_balance, False) for tx in open_transactions])
