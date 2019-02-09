e2ee1060e1ae91e078554cad8ac341b637cca9f0
David Ledvinka <david.ledvinka@post.cz>
2019-02-09 23:04:10 +0100

    added AuthGuard

--- Python/Energy/nest-energy-api/src/items/items.controller.ts
+++ Python/Energy/nest-energy-api/src/items/items.controller.ts
@@ -8,7 +8,7 @@ import {
 import { CreateItemDto } from './create-item.dto';
 import { ItemsService } from './items.service';
 import { Item } from './item.interface';
-import { ValidationPipe } from '../common/validation.pipe';
+import { ValidationPipe } from '../shared/validation.pipe';
 
 @Controller('items')
 export class ItemsController {
--- Python/Energy/nest-energy-api/src/users/users.controller.ts
+++ Python/Energy/nest-energy-api/src/users/users.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get, Post, Body, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, UsePipes, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserDto } from './user-dto';
 import { UserRO } from './user-ro';
+import { ValidationPipe } from 'src/shared/validation.pipe';
+import { AuthGuard } from 'src/shared/auth.guard';
+import { UserEntity } from './user-entity';
+import { User } from './user.decorator';
 
 @Controller('auth')
 export class UsersController {
@@ -9,18 +13,19 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Get('users')
-  async getAll(): Promise<UserRO[]> {
+  @UseGuards(new AuthGuard())
+  async getAll(@User('username') user: UserEntity): Promise<UserRO[]> {
     return this.usersService.getAll();
   }
 
   @Post('login')
-  //@UsePipes(new ValidationPipe())
+  @UsePipes(new ValidationPipe())
   login(@Body()data: UserDto) {
     return this.usersService.login(data);
   }
 
   @Post('register')
-  //@UsePipes(new ValidationPipe())
+  @UsePipes(new ValidationPipe())
   register(@Body()data: UserDto) {
     return this.usersService.register(data);
   }
